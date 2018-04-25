46f3bf3be53a245b34d9833aa1c73af8ebc7297e
Fabian Alberto Miliani <fabian.albertomiliani@plexus.local>
2018-04-25 16:52:56 +0200

    con hbs engine

--- serve.js
+++ serve.js
@@ -3,6 +3,8 @@ const app = express()
 
 app.use( express.static(__dirname +'/public' ));
 
+app.set('view engine', 'hbs');
+
 app.listen( 3000 , () => {
     console.log('escuchando peticiones  en del puerto 3000');
 })
